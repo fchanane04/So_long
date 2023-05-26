cf36dfd3c8886f5d94aabb538beca700f1998c5e
fchanane04 <93842426+fchanane04@users.noreply.github.com>
2023-05-26 23:24:08 +0100

    Cursus_Common_Core_So_long

---
+++ errors_msg.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors_msg.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchanane <fchanane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 20:10:55 by fchanane          #+#    #+#             */
+/*   Updated: 2022/06/27 22:36:50 by fchanane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"so_long.h"
+
+void	error_fd(int fd)
+{
+	if (fd < 0)
+	{
+		perror("File");
+		exit(1);
+	}
+}
+
+void	error_ext(char *file, int fd)
+{
+	if (ft_strrchr(file, '.') == NULL)
+	{
+		close(fd);
+		ft_putstr("ERROR\nFile should have an extention\n");
+		exit(1);
+	}
+	if (ft_strncmp(ft_strrchr(file, '.'), ".ber", 5))
+	{
+		close(fd);
+		ft_putstr("ERROR\nExtension is invalid\n");
+		exit(1);
+	}
+	close(fd);
+}
+
+void	error_empty(char *read, int fd)
+{
+	if (!read)
+	{
+		ft_putstr("ERROR\nMap is Empty\n");
+		close(fd);
+		exit(1);
+	}
+}
+
+void	error_equal(int fd, int size, int size_line)
+{
+	if (size_line != size)
+	{
+		ft_putstr("ERROR\nInvalid Map : Lines should be equal\n");
+		close(fd);
+		exit(1);
+	}
+}
+
+void	error_enough(int size, int lines)
+{
+	if (size < 3 || lines < 3)
+	{
+		ft_putstr("ERROR\nInvalid Map : not enough Lines and/or Columns\n");
+		exit(1);
+	}
+}
